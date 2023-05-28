5ade5cea24f14c1f4f15a6f828ce35d4e09978ca
Nasif Mustufa Khan <97841869+nasifkhan249@users.noreply.github.com>
2023-05-28 23:46:13 +0600

    add all require for server creat

---
+++ server.js
@@ -0,0 +1,29 @@
+//all require
+const { readdirSync } = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const morgan = require("morgan");
+const cors = require("cors");
+
+//application level middleware
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(helmet());
+app.use(express.urlencoded({extended: false}));
+
+//server
+const port = process.env.PORT || 8000;
+
+//Connect to DB and start server
+mongoose
+    .connect(process.env.DATABASE)
+    .then(()=>{
+        app.listen(port,()=>{
+            console.log(`Server Running on port ${port}`);
+        })
+    })
